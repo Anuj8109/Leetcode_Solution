f8075849805283b53b5d59b4a5afc874be897e8d
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2022-10-11 10:02:49 +0530

    Time: 185 ms (11.23%), Space: 61.6 MB (24.04%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int len = nums.size();
-        vector<int> v(len);
-        v[0] = nums[0];
-        for(int i=1;i<nums.size();i++){
-            v[i] = min(v[i-1],nums[i]);
-            // cout<<v[i]<<" ";
-        }
-        // cout<<endl;
-        int large = nums[len - 1];
-        for(int i = len - 2;i > 0;i--){
-            if(nums[i] >= large) large = nums[i];
-            else if(nums[i] > v[i-1]) return 1;
-            // else cout<<nums[i]<<" "<<v[i-1]<<endl;
+        int m1 = INT_MAX, m2 = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] > m1) return 1;
+            else if(nums[i] < m2) m2 = nums[i];
+            else if(nums[i] > m2 && nums[i] < m1) m1 = nums[i];
         }
         return 0;
+        // int len = nums.size();
+        // vector<int> v(len);
+        // v[0] = nums[0];
+        // for(int i=1;i<nums.size();i++){
+        //     v[i] = min(v[i-1],nums[i]);
+        //     // cout<<v[i]<<" ";
+        // }
+        // // cout<<endl;
+        // int large = nums[len - 1];
+        // for(int i = len - 2;i > 0;i--){
+        //     if(nums[i] >= large) large = nums[i];
+        //     else if(nums[i] > v[i-1]) return 1;
+        //     // else cout<<nums[i]<<" "<<v[i-1]<<endl;
+        // }
+        // return 0;
     }
 };
