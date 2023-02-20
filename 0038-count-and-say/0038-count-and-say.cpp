016bcdce4b568c6b9630cf918a3284c766b018c5
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-02-20 15:05:38 +0530

    Time: 13 ms (34.49%), Space: 6.7 MB (58.24%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string s;
-        char c = '0' + 1;
-        s.push_back(c);
-        if(n==1){
-            return s;
-        }else{
-            while(n>1){
-                char lChar = s[0];
-                string ans;
-                int i = 1;
-                int c = 0;
-                int count =1;
-                while(i<s.length()){
-                    if(s[i] != lChar){
-                        char f = '0'+ count;
-                        ans.push_back(f);
-                        ans.push_back(lChar);
-                        // cout<<count<<lChar;
-                        lChar = s[i];
-                        count = 1;
-                    }else{
-                        count++;
-                    }
-                    i++;
-                }
-                // cout<<count<<lChar<<endl;
-                char f = '0' + count;
-                ans.push_back(f);
-                ans.push_back(lChar);
-                s = ans;
-                n--;
+        string s = "1";
+        while(n>1){
+            string ans = "";
+            int i = 1;
+            int count =1;
+            while(i<s.length()){
+                if(s[i] != s[i-1]){
+                    ans += to_string(count) + s[i-1];
+                    count = 1;
+                }else count++;
+                i++;
             }
+            ans += to_string(count) + s[i-1];
+            s = ans;
+            n--;
         }
         return s;
     }
