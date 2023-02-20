05e3855f0f13e717d099a6aa7802b8e176f0e029
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-02-20 14:52:15 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (77.52%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -6,14 +6,15 @@ public:
         int s = 0,e = n-1;
         while(s <= e){
             int mid = (s + e)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target){
+            // if(nums[mid] == target) return mid;
+             if(nums[mid] < target){
                 ans = mid;
                 s = mid + 1;
             }else{
                 e = mid - 1;
             }
         }
-        return ans + 1;
+        // cout<<s<<endl;
+        return s ;
     }
 };
