490dae4b59580a4786ce1726c7274eef68cfa1f3
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-02-23 12:52:06 +0530

    Time: 287 ms (30.66%), Space: 82.1 MB (23.74%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool fun(pair<int,int> a,pair<int,int> b){
+        if(a.first == b.first) return a.second > b.second;
+        return a.first < b.first;
+    }
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> v;
+        int n = profits.size();
+        for(int i=0;i<n;i++){
+            v.push_back({capital[i],profits[i]});
+        }
+        
+        sort(v.begin(),v.end(),fun);
+        priority_queue<int> pq;
+        int index = 0;
+        // cout<<v[0].first<<" "<<v[0].second<<endl;
+        while(k > 0){
+            while(index < n && v[index].first <= w){
+                pq.push(v[index].second);
+                index++;
+            }
+            if(pq.empty()) break;
+            w += pq.top();
+            pq.pop();
+            k--;
+        }
+        return w;
+    }
+};
