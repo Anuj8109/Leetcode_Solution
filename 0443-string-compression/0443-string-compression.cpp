6686cdc8ac128a6631400ba0a4f4863490f973db
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-02 09:30:36 +0530

    Time: 6 ms (53.56%), Space: 9 MB (83.02%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int count = 0;
         int j = 0;
         int n = chars.size();
-        for(int i=0;i<chars.size();){
-            count = 0;
-            char p = chars[i];
-            while(i < n && p == chars[i]){
+        for(int i=0;i<chars.size();i++){
+            int count = 1;
+            while(i + 1 < n && chars[i+1] == chars[i]){
                 i++;
                 count++;
             }
             if(count == 1){
-                chars[j++] = p;
+                chars[j++] = chars[i];
             }else{
-                chars[j++] = p;
+                chars[j++] = chars[i];
                 auto q = to_string(count);
                 for(auto k:q) chars[j++] = k;
-                // chars[j++] = count + '0';
             }
         }
         return j;
