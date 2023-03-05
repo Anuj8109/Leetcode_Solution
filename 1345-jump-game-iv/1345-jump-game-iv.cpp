c7b7aa0e662256021c7900aefe74cb9daa04f775
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-05 19:00:07 +0530

    Time: 203 ms (86.12%), Space: 75 MB (65.30%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
+        if(arr.size() == 1) return 0;
         int n = arr.size();
         vector<bool> visit(n,0);
         unordered_map<int,vector<int>> m;
         for(int i=0;i<n;i++){
             m[arr[i]].push_back(i);
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        queue<int> q;
+        // pq.push({0,0});
+        q.push(0);
         visit[0] = 1;
         unordered_set<int> us;
-        while(!pq.empty()){
-            auto p = pq.top();
-            int steps = p.first;
-            int index = p.second;
-            visit[index] = 1;
-            pq.pop();
-            if(index == n - 1) return steps;
-            if(index - 1 > 0 && !visit[index - 1]){
-                if(index - 1 == n-1) return steps + 1;
-                pq.push({steps + 1,index - 1});
-            }
-            if(index + 1 < n && !visit[index + 1]){
-                if(index + 1 == n-1) return steps + 1;
-                pq.push({steps + 1,index + 1});
-            }
-            if(us.find(arr[index]) == us.end()){
-                us.insert(arr[index]);
-                for(auto i:m[arr[index]]){
-                    if(i == n - 1) return steps + 1;
-                    if(!visit[i]) pq.push({steps + 1,i});
+        int steps = 0;
+        while(!q.empty()){
+            int count = q.size();
+            for(int i=0;i<count;i++){
+                int index = q.front();
+                q.pop();
+                if(index - 1 > 0 && !visit[index - 1]){
+                    if(index - 1 == n-1) return steps + 1;
+                    q.push(index - 1);
+                    visit[index - 1] = 1;
+                }
+                if(index + 1 < n && !visit[index + 1]){
+                    if(index + 1 == n-1) return steps + 1;
+                    q.push(index + 1);
+                    visit[index + 1] = 1;
+                }
+                if(us.find(arr[index]) == us.end()){
+                    us.insert(arr[index]);
+                    for(auto i:m[arr[index]]){
+                        if(i == n - 1) return steps + 1;
+                        if(!visit[i]) q.push(i),visit[i] = 1;
+        
+                    }
                 }
             }
-            
-            
+            steps++;
         }
         return n;
     }
