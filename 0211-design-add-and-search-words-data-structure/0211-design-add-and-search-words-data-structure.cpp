bfda36bf5adb068049edf3175b71099fc10554cd
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-19 17:12:41 +0530

    Time: 2186 ms (20.24%), Space: 668.9 MB (5.01%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -5,12 +5,6 @@ class word{
     word(){
         v.assign(26,NULL);
     }
-    word *get(char c){
-        return v[c - 'a'];
-    }
-    void add(char c){
-        v[c - 'a'] = new word;
-    }
     
 };
 
@@ -20,32 +14,26 @@ public:
     WordDictionary() {
         root = new word;
     }
-     // WordDictionary() : root(new word) {}
     void addWord(string str) {
-        // cout<<root<<endl;
         auto p = root;
         for(auto c:str){
-            if(!p->get(c)) p->add(c);
-            p = p->get(c);
+            if(!p->v[c - 'a']) p->v[c - 'a'] = new word();
+            p = p->v[c-'a'];
         }
         p->is_leaf = true;
     }
     bool helper(string str,int i,word* root){
         if(i == str.size()) return root->is_leaf;
-        
-        if(str[i] != '.'){
-            root = root->get(str[i]);
-            // if(root->get(str[i])) return helper(str,i+1,root->get(str[i]));
-            if(root) return helper(str,i+1,root);
+        if(str[i] == '.'){
+            for(int j=0;j<26;j++){
+                auto p = root->v[j];
+                if(p && helper(str,i+1,p)) return true;
+            }
             return false;
         }
-        
-        for(int j=0;j<26;j++){
-            auto p = root->get('a' + j);
-            if(p && helper(str,i+1,p)) return true;
-            
-        }
-        return false;
+        root = root->v[str[i] - 'a'];
+        if(!root) return false;
+        return helper(str,i+1,root);
     }
     bool search(string str) {
         return helper(str,0,root);
