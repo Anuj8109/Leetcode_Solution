19f23f3a59507aa1ff5b4ccf2935fa12e0131d8a
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-24 19:02:14 +0530

    Time: 456 ms (46.48%), Space: 143.5 MB (22.38%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void helper(vector<vector<int>> &v1,vector<vector<int>> &v2,vector<bool> &visit,int &ans,int index){
+    void helper(vector<vector<vector<int>>> &graph,vector<bool> &visit,int &ans,int index){
         // if(visit[index]) return ;
         visit[index] = 1;
-        for(auto i:v2[index]){
-            if(!visit[i]) helper(v1,v2,visit,ans,i);
+        for(auto i:graph[0][index]){
+            if(!visit[i]) helper(graph,visit,ans,i);
         }
-        for(auto i:v1[index]){
+        for(auto i:graph[1][index]){
             if(!visit[i]){
                 ans++;
-                helper(v1,v2,visit,ans,i);
+                helper(graph,visit,ans,i);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& conn) {
+        vector<vector<vector<int>>> graph(2,vector<vector<int>> (n));
         vector<vector<int>> v1(n);
         vector<vector<int>> v2(n);
         vector<bool> visit(n,false);
         for(auto i:conn){
-            v1[i[0]].push_back(i[1]);
-            v2[i[1]].push_back(i[0]);
+            graph[1][i[0]].push_back(i[1]);
+            graph[0][i[1]].push_back(i[0]);
+            // v1[i[0]].push_back(i[1]);
+            // v2[i[1]].push_back(i[0]);
         }
         int ans = 0;
-        helper(v1,v2,visit,ans,0);
+        helper(graph,visit,ans,0);
         return ans;
     }
 };
