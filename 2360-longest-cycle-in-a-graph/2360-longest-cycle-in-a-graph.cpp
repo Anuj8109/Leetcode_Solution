b312ac0da4b732eb5c39b4acab887c920284dfdf
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-26 11:15:29 +0530

    Time: 758 ms (20.44%), Space: 200.7 MB (22.25%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    int count(vector<int> &edges,int start,int index){
-        if(index == start) return 1;
-        return 1 + count(edges,start,edges[index]);
-    }
-    int helper(vector<int>& edges,vector<bool> &v,unordered_map<int,int> &m,int last,int index){
-        if(m.count(index)){
+    int helper(vector<int>& edges,vector<bool> &v,map<int,int> &m,int last,int index){
+        if(m[index]){
             return last - m[index];
         }
         if(index == -1 || v[index]) return 0;
@@ -20,9 +16,9 @@ public:
         vector<bool> v(n,false);
         for(int i=0;i<n;i++){
             int count = 0;
-            unordered_map<int,int> m;
+            map<int,int> m;
             if(!v[i]){
-                ans = max(ans,helper(edges,v,m,0,i));
+                ans = max(ans,helper(edges,v,m,1,i));
             }
         }
         return ans == 0 ? -1 : ans;
