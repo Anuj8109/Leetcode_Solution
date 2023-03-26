f0a4fb31dd6f0ed9e7ab65aa96d4df79e60380b9
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-26 11:13:31 +0530

    Time: 628 ms (24.71%), Space: 221.9 MB (14.87%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -4,18 +4,14 @@ public:
         if(index == start) return 1;
         return 1 + count(edges,start,edges[index]);
     }
-    int helper(vector<int>& edges,vector<bool> &v,unordered_set<int> &s,int index){
-        if(s.find(index) != s.end()){
-            return count(edges,index,edges[index]);
+    int helper(vector<int>& edges,vector<bool> &v,unordered_map<int,int> &m,int last,int index){
+        if(m.count(index)){
+            return last - m[index];
         }
         if(index == -1 || v[index]) return 0;
-        
-        
-        
-        s.insert(index);
+        m[index] = last++;
         v[index] = 1;
-        return helper(edges,v,s,edges[index]);
-        
+        return helper(edges,v,m,last,edges[index]);
     }
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
@@ -24,9 +20,9 @@ public:
         vector<bool> v(n,false);
         for(int i=0;i<n;i++){
             int count = 0;
-            unordered_set<int> s;
+            unordered_map<int,int> m;
             if(!v[i]){
-                ans = max(ans,helper(edges,v,s,i));
+                ans = max(ans,helper(edges,v,m,0,i));
             }
         }
         return ans == 0 ? -1 : ans;
