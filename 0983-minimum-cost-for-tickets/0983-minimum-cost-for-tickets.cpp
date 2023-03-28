eb60038caaf7110c05c1db6d74bcf2cfebdf3d45
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-03-28 17:47:35 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (91.47%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(366);
+        vector<int> dp(days[n - 1] + 1);
         dp[0] = 0;
         int j = 0;
         for(int i=1;i<=365 && j < n;i++){
-            // cout<<i<<endl;
             if(i == days[j]){
-                dp[i] = dp[i-1] + costs[0];
-                dp[i] = min(dp[i],dp[max(0,i-7)] + costs[1]);
-                dp[i] = min(dp[i],dp[max(0,i-30)] + costs[2]);
+                dp[i] = min({dp[i-1] + costs[0],dp[max(0,i-7)] + costs[1],dp[max(0,i-30)] + costs[2]});
+                // dp[i] = dp[i-1] + costs[0];
+                // dp[i] = min(dp[i],);
+                // dp[i] = min(dp[i],);
                 j++;
             }else dp[i] = dp[i-1];
         }
-        return dp[days.back()];
+        return dp[days[n-1]];
         // for(int i=0;i<n;i++){
         //     int p = days[i];
         //     dp[i + 1] = dp[i] + costs[0];
