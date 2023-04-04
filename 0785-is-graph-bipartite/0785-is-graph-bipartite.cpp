de0e8366d2cdf6b23fbec05ae0fd35a87fc93b1f
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-04 16:27:51 +0530

    Time: 24 ms (77.09%), Space: 13.4 MB (87.53%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -9,14 +9,10 @@ public:
     bool dfs(vector<vector<int>>& graph,vector<bool>& visit,int i,int s){
         if(visit[i]){
             return s == 1 ? !v2[i] : !v1[i];
-            // if(s == 1 && s2.find(i) != s2.end()) return false;
-            // if(s == 2 && s1.find(i) != s1.end()) return false;
-            // return true;
         }
         // cout<<i<<" "<<s<<endl;
         visit[i] = true;
-        if(s == 1) v1[i] = 1;
-        else v2[i] = 1;
+        s == 1 ? v1[i] = 1 : v2[i] = 1; 
         int q = s == 1 ? 2 : 1;
         for(auto j:graph[i]){
             if(!dfs(graph,visit,j,q)) return false;
