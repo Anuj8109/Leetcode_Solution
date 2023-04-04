8ca51974b5b771e7a990a23137192eca3eb0fe2f
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-04 16:22:21 +0530

    Time: 25 ms (66.56%), Space: 13.8 MB (20.07%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_set<int> s1,s2;
+    bool dfs(vector<vector<int>>& graph,vector<bool>& visit,int i,int s){
+        if(visit[i]){
+            if(s == 1 && s2.find(i) != s2.end()) return false;
+            if(s == 2 && s1.find(i) != s1.end()) return false;
+            return true;
+        }
+        // cout<<i<<" "<<s<<endl;
+        visit[i] = true;
+        if(s == 1){
+            s1.insert(i);
+            // if(s2.find(i) != s2.end()) return false;
+            // else s1.insert(i);
+        }
+        // else s1.insert(i);
+        else{
+            s2.insert(i);
+            // if(s1.find(i) != s1.end()) return false;
+            // else s2.insert(i);
+        }
+        int q = s == 1 ? 2 : 1;
+        for(auto j:graph[i]){
+            if(!dfs(graph,visit,j,q)) return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<bool> visit(n,false);
+        
+        for(int i=0;i<n;i++){
+            if(!visit[i]){
+                // cout<<i<<endl;
+                if(!dfs(graph,visit,i,1)) return false;
+                // if(s1.find(i) == s1.end()){
+                //     if(!dfs(graph,visit,i,1)) return false;
+                // }
+                // else{
+                //     if(!dfs(graph,visit,i,2)) return false;
+                // }
+            }
+        }
+        return true;
+    }
+};
