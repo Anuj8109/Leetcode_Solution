889af369d4278b7eb7352dc7f839298ddd6c9974
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-04 16:26:28 +0530

    Time: 28 ms (49.31%), Space: 13.6 MB (43.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     unordered_set<int> s1,s2;
+    vector<int> v1,v2;
+    Solution(){
+        v1.assign(100,0);
+        v2.assign(100,0);
+    }
     bool dfs(vector<vector<int>>& graph,vector<bool>& visit,int i,int s){
         if(visit[i]){
-            return s == 1 ? s2.find(i) == s2.end() : s1.find(i) == s1.end();
+            return s == 1 ? !v2[i] : !v1[i];
             // if(s == 1 && s2.find(i) != s2.end()) return false;
             // if(s == 2 && s1.find(i) != s1.end()) return false;
             // return true;
         }
         // cout<<i<<" "<<s<<endl;
         visit[i] = true;
-        if(s == 1) s1.insert(i);
-        else s2.insert(i);
+        if(s == 1) v1[i] = 1;
+        else v2[i] = 1;
         int q = s == 1 ? 2 : 1;
         for(auto j:graph[i]){
             if(!dfs(graph,visit,j,q)) return false;
