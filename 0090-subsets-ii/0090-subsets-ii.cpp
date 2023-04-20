01b532190d38a1c4390afd2adf5d15a3787e5493
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-20 07:00:22 +0530

    Time: 3 ms (74.19%), Space: 7.5 MB (88.43%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    void helper(vector<int>& nums,vector<vector<int>>& ans,int index){
-        if(index >= nums.size()) return ;
-        int n = ans.size();
-        vector<vector<int>> temp;
-        int start = ans.size();
-        for(int i=0;i<n;i++){
-            // auto p = ans[i];
-            // p.push_back(nums[index]);
+    void helper(vector<int>& nums,vector<vector<int>>& ans,int index,int last){
+        if(index == nums.size()) return ;
+        // cout<<index<<" ";
+        int len = ans.size();
+        int start = 0;
+        if(index > 0 && nums[index] == nums[index - 1]) start = last;
+        // cout<<start<<endl;
+        for(int i=start;i<len;i++){
             ans[i].push_back(nums[index]);
-            // temp.push_back(p);
             ans.push_back(ans[i]);
             ans[i].pop_back();
         }
-        index++;
-        while(index < nums.size() && nums[index] == nums[index - 1]){
-            int len = ans.size();
-            for(int i=start;i<len;i++){
-                ans[i].push_back(nums[index]);
-                ans.push_back(ans[i]);
-                ans[i].pop_back();
-            }
-            start = len;
-            index++;
-        }
-        helper(nums,ans,index);
+        // index++;
+        // while(index < nums.size() && nums[index] == nums[index - 1]){
+        //     int len = ans.size();
+        //     for(int i=start;i<len;i++){
+        //         ans[i].push_back(nums[index]);
+        //         ans.push_back(ans[i]);
+        //         ans[i].pop_back();
+        //     }
+        //     start = len;
+        //     index++;
+        // }
+        helper(nums,ans,index + 1,len);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         ans.push_back({});
-        helper(nums,ans,0);
-        // for(int i=0;i<nums.size();i++){
-        //     vector<int> p = {};
-        //     helper(nums,ans,p,i);
-        // }
-        
+        helper(nums,ans,0,-1);
         return ans;
     }
 };
