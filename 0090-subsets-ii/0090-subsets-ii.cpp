d1a0819f73ff5ef45c23e05290305fc130c0b831
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-20 06:52:11 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (69.86%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,49 +4,27 @@ public:
         if(index >= nums.size()) return ;
         int n = ans.size();
         vector<vector<int>> temp;
+        int start = ans.size();
         for(int i=0;i<n;i++){
-            auto p = ans[i];
-            p.push_back(nums[index]);
-            temp.push_back(p);
-            ans.push_back(p);
+            // auto p = ans[i];
+            // p.push_back(nums[index]);
+            ans[i].push_back(nums[index]);
+            // temp.push_back(p);
+            ans.push_back(ans[i]);
+            ans[i].pop_back();
         }
         index++;
-        // cout<<nums[index]<<" "<<nums[index - 1]<<endl;
         while(index < nums.size() && nums[index] == nums[index - 1]){
-            int len = temp.size();
-            for(int i=0;i<len;i++){
-                // cout<<i.size()<<" ";
-                // &i.push_back(nums[index]);
-                // cout<<i.size()<<endl;
-                temp[i].push_back(nums[index]);
-                ans.push_back(temp[i]);
+            int len = ans.size();
+            for(int i=start;i<len;i++){
+                ans[i].push_back(nums[index]);
+                ans.push_back(ans[i]);
+                ans[i].pop_back();
             }
-            // int x = temp.size();
-            // cout<<index<<" "<<x<<endl;
-            // for(int i=0;i<x;i++){
-            //     auto p = temp[i];
-            //     p.push_back(nums[index]);
-            //     temp.push_back(p);
-            // }
+            start = len;
             index++;
-            if(index >= nums.size()) break;
         }
-        // ans.insert(ans.end(),temp.begin(),temp.end());
-        // cout<<index<<endl;
         helper(nums,ans,index);
-        // if(index == nums.size()){
-        //     if(ans.size() == 0) ans.push_back(p);
-        //     else if(ans.back() != p){
-        //         ans.push_back(p);
-        //     }
-        //     return ;
-        // }
-        
-        // helper(nums,ans,p,index+1);
-        // p.push_back(nums[index]);
-        // helper(nums,ans,p,index + 1);
-        // p.pop_back();
-        
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
