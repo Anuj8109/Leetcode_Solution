e884c5ebbbe000481cc82c16a252aefbfc9ec5e2
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-04-23 10:18:47 +0530

    Time: 125 ms (22.67%), Space: 43.2 MB (6.98%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int mod = 1e9 +  7;
+    int x;
     int helper(string &s,int k,vector<int> &dp,int index){
         if(index >= s.size()) return 1;
         else if(dp[index] != -1) return dp[index];
@@ -8,8 +9,8 @@ public:
         long p;
         long ans = 0;
         for(int i=1;i + index <= s.size();i++){
-            p = stol(s.substr(index,i));
-            if(p <= k){
+            // p = stol(s.substr(index,i));
+            if(i < x || stol(s.substr(index,i)) <= k){
                 ans = (ans + helper(s,k,dp,i + index)) % mod;
             }else break;
         }
@@ -17,6 +18,7 @@ public:
     }
     int numberOfArrays(string s, int k) {
         vector<int> dp(s.size(),-1);
+        x = to_string(k).size();
         return helper(s,k,dp,0);
     }
 };
