e3b13e9ab00de402d8e8b455d170a63edb9d7326
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2023-06-03 10:33:50 +0530

    Time: 491 ms (24.23%), Space: 157.8 MB (20.96%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    int helper(vector<int>& it,int i){
+    // vector<vector<int>> v;
+    int helper(unordered_map<int,vector<int>>& m,vector<int>& it,int i){
         // if(v[i].size() == 0) return ;
         // ans += it[i];
         int p = 0;
-        for(auto j:v[i]){
-            p = max(helper(it,j),p);
+        for(auto j:m[i]){
+            p = max(helper(m,it,j),p);
         }
         return p + it[i];
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         // vector<vector<int>> v(n);
-        v.assign(n,vector<int> {});
+        unordered_map<int,vector<int>> m;
+        // v.assign(n,vector<int> {});
         for(int i=0;i<n;i++){
             if(i != headID){
-                v[manager[i]].push_back(i);
+                m[manager[i]].push_back(i);
             }
         }
-        return helper(informTime,headID);
+        return helper(m,informTime,headID);
     }
 };
