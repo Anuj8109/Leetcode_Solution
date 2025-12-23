6d6c29cdb13590500fca602cb8718cfbc769e3b8
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2025-12-23 20:03:59 +0530

    Time: 254 ms (6.00%) | Memory: 188.3 MB (16.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -2,8 +2,7 @@ class Solution {
     public int maxTwoEvents(int[][] events) {
         Comparator<int[]> comp = (a,b) -> {
             if(a[1] == b[1]){
-                if(a[0] == b[0]) return Integer.compare(b[2],a[2]);
-                else return Integer.compare(b[0],a[0]);
+                return Integer.compare(b[0],a[0]);
             }
             return Integer.compare(a[1], b[1]);
         };
@@ -17,7 +16,6 @@ class Solution {
             int end = event[1];
             Integer prevTime = map.floorKey(start - 1);
             int totalValue = map.get(prevTime) + value;
-            // totalValue = Math.max(totalValue, map.get(map.floorKey(end)));
             int currMax = Math.max(value, map.get(map.floorKey(end)));
             // System.out.println(map.floorKey(end) + " " + map.get(map.floorKey(end)));
             ans = Math.max(totalValue, ans);
