4e602e9d5a43ed582ebe10750d0a1a81aaff8a14
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2026-01-08 20:52:35 +0530

    Time: 459 ms (5.19%) | Memory: 49 MB (8.89%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -1,12 +1,12 @@
 class Solution {
     Integer[][] dp;
-    public int maxProduct(int[] nums1, int[] nums2, int p, int q, int count){
+    public int maxProduct(int[] nums1, int[] nums2, int p, int q, boolean isEmpty){
         if(p == nums1.length || q == nums2.length){
-            if(count == 0) return Integer.MIN_VALUE>>1;
+            if(isEmpty) return Integer.MIN_VALUE>>1;
             return 0;
         }
         if(dp[p][q] != null) return dp[p][q];
-        int result = maxProduct(nums1, nums2, p + 1, q, count);
+        int result = maxProduct(nums1, nums2, p + 1, q, isEmpty);
 
         if(nums1[p] == 0){
             return Math.max(0, result);
@@ -14,7 +14,7 @@ class Solution {
 
         for(int index=q;index<nums2.length;index++){
             int curr = nums1[p] * nums2[index]
-                    + maxProduct(nums1, nums2, p + 1, index + 1, count + 1);
+                    + maxProduct(nums1, nums2, p + 1, index + 1, false);
             result = Math.max(result, curr);
         }
         return dp[p][q] = result;
@@ -23,6 +23,6 @@ class Solution {
         dp = new Integer[nums1.length][nums2.length];
 
         // for(int )
-        return maxProduct(nums1, nums2, 0, 0, 0);
+        return maxProduct(nums1, nums2, 0, 0, true);
     }
 }
