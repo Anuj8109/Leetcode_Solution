873dfc80c6aa4849bbf5f2d428788b469b252ad9
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2026-02-06 18:22:00 +0530

    Time: 36 ms (6.83%) | Memory: 86.4 MB (7.23%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPossible(int[] nums, int k, int mid){
+        for(int index=0;index+mid-1<nums.length;index++){
+            long mini = (long)nums[index] * k;
+            long maxi = nums[index + mid - 1];
+            // System.out.println("TEST " +  maxi + " " + mini + " " + Long.compare(maxi, mini));
+            if(Long.compare(maxi, mini) <= 0){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int start = 1;
+        int end = nums.length;
+        int ans = 0;
+        while(start <= end){
+            int mid = (start + end) >> 1;
+            // System.out.println(start + " " + end + " " + mid);
+            if(isPossible(nums, k, mid)){
+                ans = Math.max(ans, mid);
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        System.out.println(ans);
+        return nums.length - ans;
+    }
+}
