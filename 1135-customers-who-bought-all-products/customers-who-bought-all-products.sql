05e7374019ddc8417e2f0354ae914c220beccc7b
Anuj Chhiroliya <57958411+Anuj8109@users.noreply.github.com>
2026-02-07 10:50:47 +0530

    Time: 545 ms (57.79%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -3,12 +3,17 @@
 
 
 
-select curr.customer_id from 
-(
-select customer_id, count(distinct product_key) as count
+-- select curr.customer_id from 
+-- (
+-- select customer_id, count(distinct product_key) as count
+-- from customer 
+-- group by customer_id
+-- ) as curr
+-- where curr.count = (
+--     select count(1) 
+-- from product);
+
+select customer_id
 from customer 
 group by customer_id
-) as curr
-where curr.count = (
-    select count(1) 
-from product);
+having count(distinct product_key) = (select count(1) from product);
